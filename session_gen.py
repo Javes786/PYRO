e829a0a5145ef909c95750b405baab25b5b34167
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2022-01-04 10:13:45 +0530

    Update session_gen.py

--- session_gen.py
+++ session_gen.py
@@ -1,46 +1,17 @@
 import asyncio
-
-from configparser import ConfigParser, NoOptionError, NoSectionError
-
 import os
 
 os.system('clear')
 
 import pyrogram
-
 from pyrogram import Client
 
-config = ConfigParser()
 
-try:
-
-    config.read("config.ini")
-
-    APP_ID = config.getint("Enterprise", "APP_ID")
-
-    API_HASH = config.get("Enterprise", "API_HASH")
-
-except (NoOptionError, NoSectionError):
-
-    APP_ID = int(input("enter Telegram APP ID: "))
-
-    API_HASH = input("enter Telegram API HASH: ")
-
-    async with Client(":memory:", api_id=APP_ID, api_hash=API_HASH) as app:
-
-print(app.export_session_string())
-
-        await app.send_message(
-
-            "me", f"#STRING_SESSION\n\n```{await app.export_session_string()}``` **TAP TO COPY**"
-
+APP_ID = int(input("enter Telegram APP ID: "))
+API_HASH = input("enter Telegram API HASH: ")
+with Client(":memory:", api_id=APP_ID, api_hash=API_HASH) as app:
+        app.send_message(
+            "me",
+            f"#STRING_SESSION\n\n```{app.export_session_string()}``` **TAP TO COPY**"
         )
-
         print("Done !, session string has been sent to saved messages!")
-
-        
- if __name__ == "__main__":
-
-    loop = asyncio.get_event_loop()
-
-    loop.run_until_complete(main(APP_ID, API_HASH))     
