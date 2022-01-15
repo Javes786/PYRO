bca1eddcb51b99c8bbe6c626446f1e8da7dfe572
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2022-01-15 21:21:55 +0530

    Update SG.py

--- SG.py
+++ SG.py
@@ -13,16 +13,23 @@ hehe = """
 █▀▀ ░█░ █▀▄ █▄█ █▄█ █▀▄ █▀█ █░▀░█
 
 """
-print ("hehe")
-
+print("hehe")
 print("")
 print("")
 print("")
-APP_ID = int(input("PLEASE ENTeR Telegram APP ID: "))
-API_HASH = input("PLEASE ENTeR Telegram API HASH: ")
+print("PYROGRAM STRING SESSION GENERATOR")
+print("COPYRIGHT© BY @CRIMINAL786")
+print("100% SAFE")
+print("")
+APP_ID = int(input("PLEASE ENTER TELEGRAM APP ID: "))
+API_HASH = input("PLEASE ENTER TELEGRAM API HASH: ")
 with Client(":memory:", api_id=APP_ID, api_hash=API_HASH) as app:
         app.send_message(
             "me",
             f"#STRING_SESSION\n\n```{app.export_session_string()}``` **TAP TO COPY**"
         )
         print("Done !, session string has been sent to saved messages!")
+print("")
+print("")
+print("YOUR DETAILS ARE END TO END ENCRYPTED :)")
+print("ENJOY ❤")
