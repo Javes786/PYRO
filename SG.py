9bfed2be409543ff1af4f1efd68487783dd057d3
MOHAMMAD AMAAN <amaankhan66666@gmail.com>
2023-04-16 15:49:03 +0530

    Update SG.py

--- SG.py
+++ SG.py
@@ -34,9 +34,7 @@ API_HASH = input("PLEASE ENTER TELEGRAM API HASH: ")
 with Client(":memory:", api_id=APP_ID, api_hash=API_HASH) as app:
         app.send_message(
             "me",
-            f"#STRING_SESSION\n\n
-            ```{app.export_session_string()}```
-            **TAP TO COPY**"
+            f"```{app.export_session_string()}```**TAP TO COPY**"
         )
         print("Done !, session string has been sent to saved messages!")
 print("")
